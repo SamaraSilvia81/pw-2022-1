38e5affac8b39f563eb13500fae9b4756a0b0fa8
SamaraSilvia81 <samarasilvia3110@gmail.com>
2022-04-06 14:50:20 -0300

    update

--- Aula09/ex7/index.js
+++ Aula09/ex7/index.js
@@ -1,19 +1,40 @@
 const n1 = document.getElementById("valorA");
 const n2 = document.getElementById("valorB");
+const btCalculadora = document.getElementById("calculadora");
+const btRemove = document.getElementById("calcuRemove")
+const resultado = document.getElementById("Resultado");
 
-let resultado = document.querySelector('span');
-let resultado2 = document.querySelector('span');
-let resultado3 = document.querySelector('span');
-let resultado4 = document.querySelector('span');
-let resultado5 = document.querySelector('span');
+function geraResultado(){
 
+   const A = parseInt(n1.value);
+   const B = parseInt(n2.value);
 
-function soma(){
-   resultado.innerHTML = parseInt(n1.value) + parseInt(n2.value); 
-   resultado2.innerHTML = parseInt(n1.value) - parseInt(n2.value);
-   resultado3.innerHTML = parseInt(n1.value) * parseInt(n2.value);
-   resultado4.innerHTML = parseInt(n1.value) / parseInt(n2.value);
-   resultado5.innerHTML = parseInt(n1.value) ** parseInt(n2.value);   
+   n1.value = "";
+   n2.value = "";
+
+   let sum = A + B;
+   let sub = A - B;
+   let multi = A * B;
+   let div = A / B;
+   let pot = A ** B;
+
+   if(isNaN(A) || isNaN(B)){
+      alert("errrooo")
+   } else{
+      n1.innerHTML = "";
+      n2.innerHTML = "";
+
+      resultado.innerHTML += $sum + "\n"; 
+      resultado.innerHTML += sub + "\n";
+      resultado.innerHTML += multi + "\n";
+      resultado.innerHTML += div + "\n";
+      resultado.innerHTML += pot + "\n"; 
+   }
 }
 
-calculadora.onclick = soma;
+function remove(){
+   resultado.innerHTML = " ";
+}
+
+calculadora.onclick = geraResultado;
+calcuRemove.onclick = remove;
